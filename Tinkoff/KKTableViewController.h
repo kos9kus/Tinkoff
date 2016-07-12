6f37c1b3f3a2ec2af9aecbbe08df2fc343a5d590
Kos <kos9kus@gmail.com>
2016-07-13 01:34:12 +0300

    !

---
+++ Tinkoff/KKTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKTableViewController.h
+//  Tinkoff
+//
+//  Created by KONSTANTIN KUSAINOV on 13/07/16.
+//  Copyright © 2016 Kos9Kus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKTableViewController : UIViewController
+
+@end
---
+++ Tinkoff/KKTableViewController.m
@@ -0,0 +1,16 @@
+//
+//  KKTableViewController.m
+//  Tinkoff
+//
+//  Created by KONSTANTIN KUSAINOV on 13/07/16.
+//  Copyright © 2016 Kos9Kus. All rights reserved.
+//
+
+#import "KKTableViewController.h"
+
+
+@implementation KKTableViewController
+
+//- (instancetype)initWithContext:()
+
+@end
